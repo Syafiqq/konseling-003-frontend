b8ac9cc15ebdd8159ee37addd6348187c6ca8e27
Syafiq <syafiq.rezpector@gmail.com>
2018-11-04 21:16:28 +0700

    Register Music Player

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
+    <MusicPlayer></MusicPlayer>
     <router-view/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import MusicPlayer from '@/components/MusicPlayer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MusicPlayer
+  }
+}
+</script>
 
 <style lang="scss">
 </style>
