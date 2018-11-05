26e650e30d1fb04446cc9320a731fae568b06b6d
Syafiq <syafiq.rezpector@gmail.com>
2018-11-05 23:11:48 +0700

    Add Login COmponent

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <LoginPage></LoginPage>
+  </div>
+</template>
+
+<script>
+import LoginPage from '../../components/auth/LoginPage.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
