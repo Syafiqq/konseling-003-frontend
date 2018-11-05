10b6b2ee0832e68203d1ce15a5ccabdafc406a68
Syafiq <syafiq.rezpector@gmail.com>
2018-11-05 23:20:28 +0700

    Attach register

--- src/components/auth/LoginPage.vue
+++ src/components/auth/LoginPage.vue
@@ -40,7 +40,9 @@
                   <h2>Sign up</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                     labore et dolore magna aliqua.</p>
-                  <button class="btn btn-primary active mt-3" type="button">Register Now!</button>
+                  <router-link :to="{name:'auth-register'}" class="btn btn-primary active mt-3" type="button">Register
+                    Now!
+                  </router-link>
                 </div>
               </div>
             </div>
