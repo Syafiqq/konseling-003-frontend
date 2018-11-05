c722a25004340696a871af1127130162244d754a
Syafiq <syafiq.rezpector@gmail.com>
2018-11-05 23:12:21 +0700

    Attach router link

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-light bg-light static-top">
       <div class="container">
         <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <a class="btn btn-primary" href="#">Sign In</a>
+        <router-link :to="{ name: 'auth-login'}" class="btn btn-primary" href="javascript:void(0)">Sign In</router-link>
       </div>
     </nav>
 
