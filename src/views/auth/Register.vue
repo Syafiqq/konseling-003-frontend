50c2e161b80ba67b5c12db9c525aaa178d56cbc2
Syafiq <syafiq.rezpector@gmail.com>
2018-11-05 23:20:07 +0700

    m

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="register">
+    <RegisterPage></RegisterPage>
+  </div>
+</template>
+
+<script>
+import RegisterPage from '../../components/auth/RegisterPage.vue'
+
+export default {
+  name: 'Register',
+  components: {
+    RegisterPage
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
