a71f4167bc640555415c2aa48d84a715e6e7126a
Syafiq <syafiq.rezpector@gmail.com>
2018-11-08 06:50:58 +0700

    Add fab components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,34 @@
 <template lang="pug">
   .home
     LandingPage
+    fab(:actions='fabActions', main-icon='radio_button_checked', @music="music")
 </template>
 
 <script>
 // @ is an alias to /src
+import fab from 'vue-fab'
 import LandingPage from '@/components/LandingPage.vue'
 
 export default {
   name: 'home',
   components: {
+    fab,
     LandingPage
+  },
+  data () {
+    return {
+      fabActions: [
+        {
+          name: 'music',
+          icon: 'music_note'
+        }
+      ]
+    }
+  },
+  methods: {
+    music () {
+      this.$emit('mp-toggle-window')
+    }
   }
 }
 </script>
