f8747bb8cc87cddd75b209d341a248f320299c3f
Syafiq <syafiq.rezpector@gmail.com>
2018-11-09 04:37:15 +0700

    Register vue sweetalert

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import pace from 'pace-progress'
 import PerfectScrollbar from 'perfect-scrollbar'
+import VueSweetalert2 from 'vue-sweetalert2'
 import Cookie from 'js-cookie'
 
 require('./resources/js/bootstrap.js')
@@ -17,6 +18,7 @@ window.$.fn.perfectScrollbar = function (options) {
 }
 window.Cookies = Cookie
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2)
 
 new Vue({
   router,
