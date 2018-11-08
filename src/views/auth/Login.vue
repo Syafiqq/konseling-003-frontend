be2a0ff8944839f3a01f81780e16148528dac3dc
Syafiq <syafiq.rezpector@gmail.com>
2018-11-09 05:01:58 +0700

    Enable fab

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,15 +1,34 @@
 <template lang="pug">
   .login
     LoginPage
+    fab(:actions='fabActions', main-icon='radio_button_checked', @music="music")
 </template>
 
 <script>
 import LoginPage from '../../components/auth/LoginPage.vue'
+import fab from 'vue-fab'
+import EventBus from '../../event-bus'
 
 export default {
   name: 'Login',
   components: {
+    fab,
     LoginPage
+  },
+  data () {
+    return {
+      fabActions: [
+        {
+          name: 'music',
+          icon: 'music_note'
+        }
+      ]
+    }
+  },
+  methods: {
+    music () {
+      EventBus.$emit('mp-toggle-window')
+    }
   }
 }
 </script>
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,15 +1,34 @@
 <template lang="pug">
   .register
     RegisterPage
+    fab(:actions='fabActions', main-icon='radio_button_checked', @music="music")
 </template>
 
 <script>
 import RegisterPage from '../../components/auth/RegisterPage.vue'
+import fab from 'vue-fab'
+import EventBus from '../../event-bus'
 
 export default {
   name: 'Register',
   components: {
-    RegisterPage
+    RegisterPage,
+    fab
+  },
+  data () {
+    return {
+      fabActions: [
+        {
+          name: 'music',
+          icon: 'music_note'
+        }
+      ]
+    }
+  },
+  methods: {
+    music () {
+      EventBus.$emit('mp-toggle-window')
+    }
   }
 }
 </script>
