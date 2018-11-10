5ff5158d6191005030b0b82c1694fe13e7307fab
Syafiq <syafiq.rezpector@gmail.com>
2018-11-10 20:45:47 +0700

    Register minimizer

--- src/resources/js/nav-toggle-recorder.js
+++ src/resources/js/nav-toggle-recorder.js
@@ -1,18 +1,20 @@
-module.exports = function ($, cookies) {
+module.exports = function (container, $, cookies) {
+  container.sidebar_minimizer = () => {
+    if (cookies.get('sidebar-toggle') === 'false') {
+      cookies.set('sidebar-toggle', true)
+    } else {
+      cookies.set('sidebar-toggle', false)
+    }
+  }
+  container.navbar_minimizer = () => {
+    if (cookies.get('navbar-toggle') === 'false') {
+      cookies.set('navbar-toggle', true)
+    } else {
+      cookies.set('navbar-toggle', false)
+    }
+  }
   $(function () {
-    $('.sidebar-minimizer').on('click', function () {
-      if (cookies.get('sidebar-toggle') === 'false') {
-        cookies.set('sidebar-toggle', true)
-      } else {
-        cookies.set('sidebar-toggle', false)
-      }
-    })
-    $('.navbar-minimizer').on('click', function () {
-      if (cookies.get('navbar-toggle') === 'false') {
-        cookies.set('navbar-toggle', true)
-      } else {
-        cookies.set('navbar-toggle', false)
-      }
-    })
+    $('.sidebar-minimizer').on('click', container.sidebar_minimizer)
+    $('.navbar-minimizer').on('click', container.sidebar_minimizer)
   })
 }
