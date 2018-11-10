9e52d292346cf4b7987aef86bd1244caa149e6d8
Syafiq <syafiq.rezpector@gmail.com>
2018-11-10 12:31:08 +0700

    Remove jquery components

--- src/resources/js/nav-toggle-navigator.js
+++ src/resources/js/nav-toggle-navigator.js
@@ -1,25 +1,23 @@
-module.exports = function ($, document, cookies) {
-  $(function () {
-    if (!document.body.classList.contains('sidebar-fixed sidebar-lg-show')) {
-      if (cookies != null) {
-        if (cookies.get('sidebar-toggle') == null) {
-        } else if (cookies.get('sidebar-toggle') === 'true') {
-          document.body.classList.remove('brand-minimized')
-          document.body.classList.remove('sidebar-minimized')
-        } else {
-          document.body.classList.add('brand-minimized')
-          document.body.classList.add('sidebar-minimized')
-        }
-        if (cookies.get('navbar-toggle') == null) {
-        } else if (cookies.get('navbar-toggle') === 'false') {
-          document.body.classList.remove('sidebar-lg-show')
-        } else {
-          document.body.classList.add('sidebar-lg-show')
-        }
+module.exports = function (document, cookies) {
+  if (!document.body.classList.contains('sidebar-fixed sidebar-lg-show')) {
+    if (cookies != null) {
+      if (cookies.get('sidebar-toggle') == null) {
+      } else if (cookies.get('sidebar-toggle') === 'true') {
+        document.body.classList.remove('brand-minimized')
+        document.body.classList.remove('sidebar-minimized')
+      } else {
+        document.body.classList.add('brand-minimized')
+        document.body.classList.add('sidebar-minimized')
       }
-    } else {
-      document.body.classList.remove('brand-minimized')
-      document.body.classList.remove('sidebar-minimized')
+      if (cookies.get('navbar-toggle') == null) {
+      } else if (cookies.get('navbar-toggle') === 'false') {
+        document.body.classList.remove('sidebar-lg-show')
+      } else {
+        document.body.classList.add('sidebar-lg-show')
+      }
     }
-  })
+  } else {
+    document.body.classList.remove('brand-minimized')
+    document.body.classList.remove('sidebar-minimized')
+  }
 }
