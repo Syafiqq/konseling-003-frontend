ec32e79233a44897a483b04145b78dbd4e466d65
Syafiq <syafiq.rezpector@gmail.com>
2018-11-11 20:39:21 +0700

    Add default dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-  .dashboard
+  .animated.fadeIn
+    b-row
+      b-col(md='12')
+        b-card(header='Dashboard' footer='Dashboard')
+          | Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
 </template>
 
 <script>
