2c20d634940a3ae505793a5c63a7ea6225b3d0b4
Syafiq <syafiq.rezpector@gmail.com>
2018-11-18 10:25:03 +0700

    Fix Login Page

--- src/components/auth/LoginPage.vue
+++ src/components/auth/LoginPage.vue
@@ -7,37 +7,43 @@
             .card.p-4
               .card-body
                 h1 Login
-                p.text-muted Sign In to your account
+                p.text-muted Login untuk memulai aplikasi
                 .input-group.mb-3
                   .input-group-prepend
                     span.input-group-text
                       i.icon-user
-                  input.form-control(type='text', placeholder='Username')
+                  input.form-control(type='text', placeholder='NISN' :value='auth.credential')
                 .input-group.mb-4
                   .input-group-prepend
                     span.input-group-text
                       i.icon-lock
-                  input.form-control(type='password', placeholder='Password')
+                  input.form-control(type='password', placeholder='Password' :value='auth.password')
                 .row
                   .col-6
                     button.btn.btn-primary.px-4(type='button') Login
                   .col-6.text-right
-                    button.btn.btn-link.px-0(type='button') Forgot password?
+                    button.btn.btn-link.px-0(type='button') Lupa password?
             .card.text-white.bg-primary.py-5.d-md-down-none(style='width:44%')
               .card-body.text-center
                 div
-                  h2 Sign up
+                  h2 Daftar
                   p
-                    | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                    | labore et dolore magna aliqua.
+                    | Segera daftarkan diri anda untuk dapat menggunakan aplikasi ini
                   router-link.btn.btn-primary.active.mt-3(:to="{name:'auth-register'}", type='button')
-                    | Register
-                    | Now!
+                    | Daftar Sekarang !
 </template>
 
 <script>
 export default {
-  name: 'LoginPage'
+  name: 'LoginPage',
+  data () {
+    return {
+      auth: {
+        credential: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
