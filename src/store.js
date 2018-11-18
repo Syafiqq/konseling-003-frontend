8cc34245c8942c92a3b1019af99d63bc3a417d78
Syafiq <syafiq.rezpector@gmail.com>
2018-11-18 15:06:27 +0700

    Enhance store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from 'router'
+import axios from 'scripts/services/http/refreshable-axios-service'
 
 Vue.use(Vuex)
 
+const LOGIN = 'LOGIN'
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+const LOGOUT = 'LOGOUT'
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    isLoggedIn: localStorage.getItem('token')
+  },
+  mutations: {
+    [LOGIN] (state) {
+      state.pending = true
+    },
+    [LOGIN_SUCCESS] (state, token) {
+      localStorage.setItem('token', token)
+      axios.refresh(token)
+
+      state.isLoggedIn = token
+      state.pending = false
+    },
+    [LOGOUT] (state) {
+      localStorage.removeItem('token')
+      axios.refresh(null)
+
+      state.isLoggedIn = null
+    }
+  },
+  actions: {
+    login ({ state, commit, rootState }, credential) {
+      console.log(['state', state, 'commit', commit, 'rootState', rootState, 'creds', credential])
+      commit(LOGIN)
+      return new Promise(resolve => {
+        commit(LOGIN_SUCCESS, credential.token)
+        resolve()
+      })
+    },
+    logout ({ commit }, callback = () => router.push('/')) {
+      return new Promise(resolve => {
+        commit(LOGOUT)
+        resolve()
+      }).then(callback)
+    }
+  }
 })
