aac07343280a6381d9bbd5354e54d0602b4d5807
Syafiq <syafiq.rezpector@gmail.com>
2018-11-18 19:02:46 +0700

    Fix login service

--- src/scripts/services/auth/login-service.js
+++ src/scripts/services/auth/login-service.js
@@ -1,9 +1,22 @@
 import axios from '../http/common-axios'
+import store from '../../../store'
+import router from '../../../router'
 
 export default function (credentials, success, failed, always) {
   return axios
     .post('/student/auth/login', credentials)
-    .then(success)
+    .then((response) => {
+      if (response.status === 200) {
+        store.dispatch('login', {
+          token: response.data.data.token
+        }).then(() => {
+          router.push({ name: 'home' })
+        })
+      }
+      if (success != null) {
+        success(response)
+      }
+    })
     .catch(failed)
     .then(always)
 }
