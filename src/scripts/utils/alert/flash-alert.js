8f74426cbdd6854895e1202faa9684dcf741293d
Syafiq <syafiq.rezpector@gmail.com>
2018-11-20 01:36:41 +0700

    Add flash alert

---
+++ src/scripts/utils/alert/flash-alert.js
@@ -0,0 +1,8 @@
+import commonAlert from 'common-alert'
+import store from '../../../store'
+
+export default function () {
+  let alerts = store.getters.alert
+  commonAlert(alerts || [])
+  store.commit('FLASH_ALERT', [])
+}
