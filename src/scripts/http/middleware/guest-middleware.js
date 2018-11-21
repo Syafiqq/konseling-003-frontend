19de315946584588c1632439f8840c40560cb2c4
Syafiq <syafiq.rezpector@gmail.com>
2018-11-22 05:53:38 +0700

    Implement ping server

--- src/scripts/http/middleware/guest-middleware.js
+++ src/scripts/http/middleware/guest-middleware.js
@@ -1,26 +1,8 @@
-import PingService from '../../services/auth/ping-service'
 import store from '../../../store'
+import pingServer from '../../services/http/ping-server'
 
 export default async function (to, from, next) {
-  let token = store.state.token
-  console.log({ token: token, to: to, from: from })
-  let status = false
-  if (token != null) {
-    let response = null
-    await PingService(
-      (success) => {
-        response = success
-      },
-      (failed) => {
-        response = failed.response
-      }
-    )
-    if (response != null && response.status === 200) {
-      status = true
-    } else {
-      await store.dispatch('purge_token')
-    }
-  }
+  let status = await pingServer()
 
   if (!status) {
     next()
