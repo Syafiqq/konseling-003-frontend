802a27c70d1cf52a91d6f3b796db02a7da7eeea3
Syafiq <syafiq.rezpector@gmail.com>
2018-11-22 04:10:32 +0700

    remove instance key

--- src/scripts/services/http/refreshable-axios-service.js
+++ src/scripts/services/http/refreshable-axios-service.js
@@ -10,7 +10,6 @@ axiosCommonConfig(instance)
 const { success, error } = interceptor
 const rAxios = {
   interceptor: instance.interceptors.response.use(success, error),
-  instance: instance,
   refresh: (token) => {
     instance.defaults.headers.post['Authorization'] = `Bearer ${token}`
   },
