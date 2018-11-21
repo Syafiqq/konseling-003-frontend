f386954f3e3eb540741409590326aea9acca9740
Syafiq <syafiq.rezpector@gmail.com>
2018-11-22 04:36:30 +0700

    fix relash alert

--- src/scripts/utils/alert/flash-alert.js
+++ src/scripts/utils/alert/flash-alert.js
@@ -1,10 +1,12 @@
 import commonAlert from './common-alert'
 import store from '../../../store'
 
-export default function (reflash = false) {
+export default function () {
   let alerts = store.state.alert
   commonAlert(alerts || [])
-  if (!reflash) {
+  if (!store.state.reflashAlert) {
     store.commit('FLASH_ALERT', [])
+    return
   }
+  store.commit('REFLASH_ALERT', false)
 }
