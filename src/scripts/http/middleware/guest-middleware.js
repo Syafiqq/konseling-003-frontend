419e9ed77b71a6bf64d188b45f432ae6beb48d57
Syafiq <syafiq.rezpector@gmail.com>
2018-11-22 23:13:41 +0700

    Fix message

--- src/scripts/http/middleware/guest-middleware.js
+++ src/scripts/http/middleware/guest-middleware.js
@@ -1,13 +1,19 @@
 import store from '../../../store'
+import commonAlert from '../../utils/alert/common-alert'
 import pingServer from '../../services/http/ping-server'
+import router from '../../../router'
 
 export default async function (to, from, next) {
   let status = await pingServer()
-
   if (!status) {
     next()
   } else {
-    store.commit('FLASH_ALERT', ['Anda Sudah Terauthentikasi'])
+    let alert = ['Anda Sudah Terauthentikasi']
+    if (router.currentRoute?.path === from.path) {
+      commonAlert(alert)
+    } else {
+      store.commit('FLASH_ALERT', alert)
+    }
     next(from ? from.path : '/')
   }
 }
