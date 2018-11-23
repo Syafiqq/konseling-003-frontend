fa935ab1ac27797d09b6d726d91a8bddffc1bf7f
Syafiq <syafiq.rezpector@gmail.com>
2018-11-23 23:35:22 +0700

    Add Recover Page

---
+++ src/views/auth/Recover.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .recover
+    RecoverPage
+    fab(:actions='fabActions', main-icon='radio_button_checked', @music="music")
+</template>
+
+<script>
+import RecoverPage from '../../components/auth/RecoverPage.vue'
+import fab from 'vue-fab'
+import EventBus from '../../scripts/utils/event-bus'
+
+export default {
+  name: 'Recover',
+  components: {
+    RecoverPage,
+    fab
+  },
+  data () {
+    return {
+      fabActions: [
+        {
+          name: 'music',
+          icon: 'music_note'
+        }
+      ]
+    }
+  },
+  methods: {
+    music () {
+      EventBus.$emit('mp-toggle-window')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
