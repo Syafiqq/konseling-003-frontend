0e4f05f097382337bb3ba3ee83649e33d2610a16
Syafiq <syafiq.rezpector@gmail.com>
2018-11-24 06:21:45 +0700

    Add dashboard guard

---
+++ src/scripts/http/guard/dashboard-guard.js
@@ -0,0 +1,32 @@
+import store from '../../../store'
+import service from '../../services/dashboard-service'
+import router from '../../../router'
+import commonAlert from '../../utils/alert/common-alert'
+
+export default async function (to, from, next) {
+  let data = {}
+  let alert = []
+  let status = false
+  await service((rSuccess) => {
+    if (rSuccess.status === 200) {
+      data = rSuccess?.data?.data || {}
+      status = true
+    }
+    alert = rSuccess?.data?.alert || []
+  }, (rFailed) => {
+    alert = rFailed?.response?.data?.alert || []
+  })
+  if (!status) {
+    store.commit('FLASH_ALERT', alert)
+    next(vm => {
+      vm.setData(data)
+    })
+  } else {
+    if (router.currentRoute?.path === from.path) {
+      commonAlert(alert)
+    } else {
+      store.commit('FLASH_ALERT', alert)
+    }
+    next(from && from.path === '/' ? '/' : false)
+  }
+}
