1251d17f74c983d3b614f36f759d48d8a7490273
Syafiq <syafiq.rezpector@gmail.com>
2018-11-24 05:35:56 +0700

    Add delete and get

--- src/scripts/services/http/axios-common-config.js
+++ src/scripts/services/http/axios-common-config.js
@@ -5,4 +5,8 @@ export default (axios) => {
   axios.defaults.headers.post['Accept'] = 'application/json'
   axios.defaults.headers.patch['Content-Type'] = 'application/json;charset=UTF-8'
   axios.defaults.headers.patch['Accept'] = 'application/json'
+  axios.defaults.headers.delete['Content-Type'] = 'application/json;charset=UTF-8'
+  axios.defaults.headers.delete['Accept'] = 'application/json'
+  axios.defaults.headers.get['Content-Type'] = 'application/json;charset=UTF-8'
+  axios.defaults.headers.get['Accept'] = 'application/json'
 }
