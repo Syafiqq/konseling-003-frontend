339f8f2aa0001844795d40d7a9f0b409aba8fe04
Syafiq <syafiq.rezpector@gmail.com>
2018-11-23 23:27:49 +0700

    Add lost view

---
+++ src/views/auth/Lost.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .lost
+    LostPage
+    fab(:actions='fabActions', main-icon='radio_button_checked', @music="music")
+</template>
+
+<script>
+import LostPage from '../../components/auth/LostPage.vue'
+import fab from 'vue-fab'
+import EventBus from '../../scripts/utils/event-bus'
+
+export default {
+  name: 'Lost',
+  components: {
+    LostPage,
+    fab
+  },
+  data () {
+    return {
+      fabActions: [
+        {
+          name: 'music',
+          icon: 'music_note'
+        }
+      ]
+    }
+  },
+  methods: {
+    music () {
+      EventBus.$emit('mp-toggle-window')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
