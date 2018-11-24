6e10055aa79a0a329225504b19853e30c0ad9c02
Syafiq <syafiq.rezpector@gmail.com>
2018-11-25 06:48:53 +0700

    move profile get service

--- src/scripts/http/guard/profile-get-guard.js
+++ src/scripts/http/guard/profile-get-guard.js
@@ -1,4 +1,4 @@
-import service from '../../services/profile-get-service'
+import service from '../../services/profile/profile-get-service'
 import guard from './common-guard'
 
 export default async function (to, from, next) {
--- src/scripts/services/profile/profile-get-service.js
+++ src/scripts/services/profile/profile-get-service.js
@@ -1,4 +1,4 @@
-import service from './common-get-service'
+import service from '../common-get-service'
 
 export default function (success, failed, always) {
   return service('/student/profile/', success, failed, always)
