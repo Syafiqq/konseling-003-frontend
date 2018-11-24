e044b734622aa8ad65b8cf4a4dc9919e53487aa5
Syafiq <syafiq.rezpector@gmail.com>
2018-11-25 06:44:27 +0700

    cleaning dashboard get guard

--- src/scripts/http/guard/dashboard-guard.js
+++ src/scripts/http/guard/dashboard-guard.js
@@ -1,30 +1,6 @@
-import store from '../../../store'
-import service from '../../services/dashboard-service'
-import router from '../../../router'
-import commonAlert from '../../utils/alert/common-alert'
+import service from '../../services/dashboard-get-service'
+import guard from './common-guard'
 
 export default async function (to, from, next) {
-  let data = {}
-  let alert = []
-  let status = false
-  await service((rSuccess) => {
-    if (rSuccess.status === 200) {
-      data = rSuccess?.data?.data || {}
-      status = true
-    }
-    alert = rSuccess?.data?.alert || []
-  }, (rFailed) => {
-    alert = rFailed?.response?.data?.alert || []
-  })
-  if (status) {
-    store.commit('FLASH_ALERT', alert)
-    next(vm => vm.setData(data))
-  } else {
-    if (router.currentRoute?.path === from.path) {
-      commonAlert(alert)
-    } else {
-      store.commit('FLASH_ALERT', alert)
-    }
-    next(from && from.path === '/' ? '/' : false)
-  }
+  await guard(service, to, from, next)
 }
