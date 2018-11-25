5cba5ce988b5a3640fa49a7d1b25ca66ee3ef3b1
Syafiq <syafiq.rezpector@gmail.com>
2018-11-25 17:32:58 +0700

    Fix question

--- src/components/course/StartPage.vue
+++ src/components/course/StartPage.vue
@@ -2,19 +2,16 @@
   b-row
     b-col(md='12')
       b-card
-        .form-group
-          p
-            h3(v-if=cQuestion)
-              strong {{cQuestion}}
         .form-group(v-for='option in cOptions')
           .pretty.p-default.p-smooth.font-xl
             input.form-control(type='radio', :value='option.id' v-model='answer')
             .state.p-info
               label {{option.description}}
-        b-progress.mb-2(:max='100')
-          b-progress-bar(heght='1.5rem' :value='cProgress', variant='info', striped='', :animated='true')
+        b-progress.mb-2(height='1.4rem', :max='100')
+          b-progress-bar(:value='cProgress', variant='info', striped='', :animated='true')
             strong.text-dark {{cProgress.toFixed(2)}} %
-        h3.mb-0(slot='header') Pertanyaan
+        h3.mb-0(slot='header')
+          strong {{cQuestion}}
         div.center-text-align(slot='footer')
           button.align-content-center.btn.btn-sm.float-left.px-4(:class="cPrevAvailable ? 'btn-primary' : 'btn-danger'" type='button', @click='doPrev' :disabled='!cPrevAvailable') Sebelumnya
           button.align-content-center.btn.btn-primary.px-4(type='button', @click='doAnswer' :disabled='isDisabled') Jawab
