5111c6c0495d56577144fc4aa7453c50861ed1c5
Syafiq <syafiq.rezpector@gmail.com>
2018-11-28 05:43:15 +0700

    Add result page

---
+++ src/components/course/ResultPage.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div
+</template>
+
+<script>
+
+export default {
+  name: 'CourseResultPage',
+  props: {
+    result: Object
+  },
+  data () {
+    return {
+      answer: null,
+      disabled: false
+    }
+  },
+  methods: {},
+  computed: {
+    isDisabled: function () {
+      return this.disabled
+    },
+    cCategories () {
+      return this.$props.result.categories || ''
+    },
+    cAnswers () {
+      return this.$props.result.answers || []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
