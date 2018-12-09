63cfad40739203c27411e5ac369646052615aaa6
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 09:27:18 +0700

    Remove log

--- src/scripts/services/course/course-detail-get-service.js
+++ src/scripts/services/course/course-detail-get-service.js
@@ -1,6 +1,5 @@
 import service from '../http/common-get-service'
 
 export default function (to, success, failed, always) {
-  console.log(to?.params)
   return service(`/student/course/${to?.params['id']}/detail`, success, failed, always)
 }
