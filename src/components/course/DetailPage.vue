d4bdf93575acd15e66f5ae280c327376b4823147
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 08:55:17 +0700

    Add detail page

---
+++ src/components/course/DetailPage.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  b-row
+    b-col(md='12')
+      b-card
+        h3.mb-0(slot='header')
+        div.center-text-align(slot='footer')
+</template>
+
+<script>
+
+export default {
+  name: 'CourseDetailPage',
+  props: {
+    student: Object
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+  watch: {},
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
