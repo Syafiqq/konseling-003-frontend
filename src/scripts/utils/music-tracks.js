27332c6483d73aef5618f949c37a34889e2ae122
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 16:56:26 +0700

    Fix song

--- src/scripts/utils/music-tracks.js
+++ src/scripts/utils/music-tracks.js
@@ -1,38 +1,32 @@
 module.exports = [
   {
     autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
+    title: 'Truthfulness 1',
+    artist: 'Truthfulness 1',
+    src: require('../../assets/song/truth1.mp3')
   },
   {
     autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
+    title: 'Truthfulness 2',
+    artist: 'Truthfulness 2',
+    src: require('../../assets/song/truth2.mp3')
   },
   {
     autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
+    title: 'Truthfulness 3',
+    artist: 'Truthfulness 3',
+    src: require('../../assets/song/truth3.mp3')
   },
   {
     autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
+    title: 'Truthfulness 4',
+    artist: 'Truthfulness 4',
+    src: require('../../assets/song/truth4.mp3')
   },
   {
     autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
-  },
-  {
-    autoplay: true,
-    title: 'Samnple Audio',
-    artist: 'Samnple Audio',
-    src: require('../../assets/song/sample-audio.mp3')
+    title: 'Truthfulness 5',
+    artist: 'Truthfulness 5',
+    src: require('../../assets/song/truth5.mp3')
   }
 ]
