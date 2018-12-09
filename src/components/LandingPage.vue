b126c92f4aafaa0ad8d998601c93b38ddfc5a80b
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 14:54:11 +0700

    Fixing landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -49,7 +49,13 @@
                 button.btn.btn-block.btn-lg.btn-primary(@click="proceedRegister") Masuk!
     section.showcase
       .container-fluid.p-0
-        .row.m-3.align-items-center
+        .row.mt-5
+          .col-md-12.text-center
+            h1.section-title PROFIL PENGEMBANG
+            .section-title-divider
+            br
+            h2.section-description Dosen dan Asisten Dosen Pembimbing
+        .row.m-5.align-items-center
           .col-lg-3.text-center
             img.img-thumbnail.img-fluid.rounded-circle.mb-3(width=250, src='../assets/img/landing-page/prof.jpg', alt='')
             h4 Prof. Dr. Hj. Nur Hidayah, M.Pd
@@ -59,101 +65,130 @@
               .card-body
                 p.card-text
                   .quote
-                    b
-                      img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
-                    |  Lahir di Gresik, 17 Agustus 1959 dan Bertempat tinggal di Perum Permata Hijau D/57, Kota Malang. Memiliki Hobby Membaca dan Motto &ldquo; Be Your Self&rdquo;. Telah selesai menempuh Pendidikan S-1 jurusan Bimbingan dan Konseling dan sedang menyelesaikan studi S-2 Bimbingan dan Konseling Universitas Negeri Malang
-                    b
-                      img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
+                    img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
+                    strong
+                      |  Lahir di Gresik, 17 Agustus 1959 dan Bertempat tinggal di Perum Permata Hijau D/57, Kota Malang. Memiliki Hobby Membaca dan Motto &ldquo; Be Your Self&rdquo;. Telah selesai menempuh Pendidikan S-1 jurusan Bimbingan dan Konseling dan sedang menyelesaikan studi S-2 Bimbingan dan Konseling Universitas Negeri Malang
+                    img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
                     br
                     br
-                    |  email: nur.hidayah.fip@gmail.com
+                    i.far.fa-envelope
+                    |  : nur.hidayah.fip@gmail.com
                     br
-                    |  phone: 082132852538
-        .row.no-gutters
-          .col-lg-6.order-lg-2.text-white.showcase-img.showcase-1
-          .col-lg-6.order-lg-1.my-auto.showcase-text
-            h2 Fully Responsive Design
-            p.lead.mb-0
-              | When you use a theme created by Start Bootstrap, you know that the theme will look
-              | great on any device, whether it&apos;s a phone, tablet, or desktop the page will behave responsively!
-        .row.no-gutters
-          .col-lg-6.text-white.showcase-img.showcase-2
-          .col-lg-6.my-auto.showcase-text
-            h2 Updated For Bootstrap 4
-            p.lead.mb-0
-              | Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in
-              | mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!
-        .row.no-gutters
-          .col-lg-6.order-lg-2.text-white.showcase-img.showcase-3
-          .col-lg-6.order-lg-1.my-auto.showcase-text
-            h2 Easy to Use &amp; Customize
-            p.lead.mb-0
-              | Landing Page is just HTML and CSS with a splash of SCSS for users who demand some
-              | deeper customization options. Out of the box, just add your content and images, and your new landing page
-              | will be ready to go!
+                    i.fas.fa-phone
+                    |  : 082132852538
+        .row.m-5.align-items-center
+          .col-lg-9
+            .card.align-middle
+              .card-body
+                p.card-text
+                  .quote
+                    img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
+                    strong
+                      |  Lahir di Samarinda, 20 Juni 1993 dan Bertempat tinggal di Jl. K. H. Wahid Hasyim No.52 Rt. 08 Samarinda, Kalimantan Timur. Memiliki Hobby Traveling dan Motto “Untuk mendapatkan kesuksesan, keberanianmu harus lebih besar daripada ketakutanmu”. Telah selesai menempuh Pendidikan S-1 jurusan Bimbingan dan Konseling dan sedang menyelesaikan studi S-2 Bimbingan dan Konseling di Universitas Megeri Malang
+                    img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
+                    br
+                    br
+                    i.far.fa-envelope
+                    |  : yunistia.tia@gmail.com
+                    br
+                    i.fas.fa-phone
+                    |  : 0895637859644
+          .col-lg-3.text-center
+            img.img-thumbnail.img-fluid.rounded-circle.mb-3(width=250, src='../assets/img/landing-page/b.jpg', alt='')
+            h4 Yunistia Prahastini, S. Pd
+            span Asisten Dosen Pembimbing
+        .row.mt-5
+          .col-md-12.text-center
+            h2.section-description Tim Pengembang Inventory
+              i Truthfulness
+        .row.m-5.align-items-center
+          .col-lg-3.text-center
+            img.img-thumbnail.img-fluid.rounded-circle.mb-3(width=250, src='../assets/img/landing-page/1.jpg', alt='')
+            h4 Ananda Riskiya Nur Isnaini
+            span Mahasiswa
+          .col-lg-9
+            .card.align-middle
+              .card-body
+                p.card-text
+                  .quote
+                    img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
+                    strong
+                      |  Lahir di Bondowoso, 23 November 1998 dan Bertempat tinggal di Jl. Tamanan Grujugan Lor Jambesari, Bondowoso. Memiliki Hobby Travelling dan Motto “DUIT (Do’a Usaha Ikhtiar Tawakal)”.
+                    img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
+                    br
+                    br
+                    i.far.fa-id-card
+                    |  : 170111600016
+                    br
+                    i.far.fa-envelope
+                    |  : anandarizkiyaa@gmail.com
+                    br
+                    i.fas.fa-phone
+                    |  : 089656764704
+        .row.m-5.align-items-center
+          .col-lg-9
+            .card.align-middle
+              .card-body
+                p.card-text
+                  .quote
+                    img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
+                    strong
+                      |  Lahir di Malang, 04 Agustus 1998 dan Bertempat tinggal di Jl. Sultan Agung (Sempol) Rt. 01 Rw. 05 Singosari, Malang. Memiliki Hobby Mencari Hal Baru dan Motto “Insya Allah Nothing is Impossible”.
+                    img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
+                    br
+                    br
+                    i.far.fa-id-card
+                    |  : 170111600020
+                    br
+                    i.far.fa-envelope
+                    |  : ovitadewi4@gmail.com
+                    br
+                    i.fas.fa-phone
+                    |  : 089681006152
+          .col-lg-3.text-center
+            img.img-thumbnail.img-fluid.rounded-circle.mb-3(width=250, src='../assets/img/landing-page/2.jpg', alt='')
+            h4 Ovita Dewi Sandra Wati
+            span Mahasiswa
+        .row.m-5.align-items-center
+          .col-lg-3.text-center
+            img.img-thumbnail.img-fluid.rounded-circle.mb-3(width=250, src='../assets/img/landing-page/3.jpg', alt='')
+            h4 Yustina Novi Erniasari
+            span Mahasiswa
+          .col-lg-9
+            .card.align-middle
+              .card-body
+                p.card-text
+                  .quote
+                    img(src='../assets/img/landing-page/quote_sign_left.png', alt='')
+                    strong
+                      |  Lahir di Kediri ,08 November 1998 dan Bertempat tinggal di Jl.Nakula, Dsn. Brenjuk, Desa Purwodadi RT 02 RW 02 Kecamatan Kras, Kab. Kediri. Memiliki Hobby Travelling dan membaca, serta memiliki Motto “Man Shobaro Zafiro (Siapa yang bersabar akan beruntung)”.
+                    img(src='../assets/img/landing-page/quote_sign_right.png', alt='')
+                    br
+                    br
+                    i.far.fa-id-card
+                    |  : 170111600116
+                    br
+                    i.far.fa-envelope
+                    |  : yustinanovierniasari@gmail.com
+                    br
+                    i.fas.fa-phone
+                    |  : 085708614773
     section.testimonials.text-center.bg-light
       .container
-        h2.mb-5 What people are saying...
+        h2.mb-5 Tim Pengembang Aplikasi
         .row
-          .col-lg-4
-            .testimonial-item.mx-auto.mb-5.mb-lg-0
-              img.img-fluid.rounded-circle.mb-3(src='../assets/img/landing-page/testimonials-1.jpg', alt='')
-              h5 Margaret E.
-              p.font-weight-light.mb-0 &quot;This is fantastic! Thanks so much guys!&quot;
-          .col-lg-4
-            .testimonial-item.mx-auto.mb-5.mb-lg-0
-              img.img-fluid.rounded-circle.mb-3(src='../assets/img/landing-page/testimonials-2.jpg', alt='')
-              h5 Fred S.
+          .col-lg-6
+            .testimonial-item.mx-auto.mb-8.mb-lg-0
+              img.img-fluid.rounded-circle.mb-3(src='../assets/img/landing-page/s.png', alt='')
+              h5 Muhammad Syafiq, S.Kom
               p.font-weight-light.mb-0
-                | &quot;Bootstrap is amazing. I&apos;ve been using it to create lots of super nice
-                | landing pages.&quot;
-          .col-lg-4
-            .testimonial-item.mx-auto.mb-5.mb-lg-0
-              img.img-fluid.rounded-circle.mb-3(src='../assets/img/landing-page/testimonials-3.jpg', alt='')
-              h5 Sarah W.
-              p.font-weight-light.mb-0 &quot;Thanks so much for making these free resources available to us!&quot;
-    // Call to Action
-    section.call-to-action.text-white.text-center
-      .overlay
-      .container
-        .row
-          .col-xl-9.mx-auto
-            h2.mb-4 Ready to get started? Sign up now!
-          .col-md-10.col-lg-8.col-xl-7.mx-auto(v-if='!isLoggedIn')
-            .form-row
-              .col-12.col-md-9.mb-2.mb-md-0
-                input.form-control.form-control-lg(type='email', placeholder='Tuliskan NISN Anda', v-model='authPass.credential')
-              .col-12.col-md-3
-                button.btn.btn-block.btn-lg.btn-primary(@click="proceedRegister") Daftar!
-    // Footer
-    footer.footer.bg-light
-      .container
-        .row
-          .col-lg-6.h-100.text-center.text-lg-left.my-auto
-            ul.list-inline.mb-2
-              li.list-inline-item
-                a(href='javascript:void(0)') About
-              li.list-inline-item &sdot;
-              li.list-inline-item
-                a(href='javascript:void(0)') Contact
-              li.list-inline-item &sdot;
-              li.list-inline-item
-                a(href='javascript:void(0)') Terms of Use
-              li.list-inline-item &sdot;
-              li.list-inline-item
-                a(href='javascript:void(0)') Privacy Policy
-            p.text-muted.small.mb-4.mb-lg-0 &copy; Your Website 2018. All Rights Reserved.
-          .col-lg-6.h-100.text-center.text-lg-right.my-auto
-            ul.list-inline.mb-0
-              li.list-inline-item.mr-3
-                a(href='javascript:void(0)')
-                  i.fab.fa-facebook.fa-2x.fa-fw
-              li.list-inline-item.mr-3
-                a(href='javascript:void(0)')
-                  i.fab.fa-twitter-square.fa-2x.fa-fw
-              li.list-inline-item
-                a(href='javascript:void(0)')
-                  i.fab.fa-instagram.fa-2x.fa-fw
+                |  Lahir di Pasuruan, 16 Desember 1993 dan beralamat di Jl. Apel I No. 449 Bangil, Pasuruan. Memiliki Motto "Hidup Seperti LARRY, Hidup Seperti LARRY". Telah menyelesaikan Pendidikan S-1 Teknik Informatika di Universitas Brawijaya.
+          .col-lg-6
+            .testimonial-item.mx-auto.mb-8.mb-lg-0
+              img.img-fluid.rounded-circle.mb-3(src='../assets/img/landing-page/h.jpg', alt='')
+              h5 Husni Hanafi, M.Pd
+              p.font-weight-light.mb-0
+                |  Lahir di Pasuruan, 19 Juni 1994 dan beralamat di Jl. Suro No. 40 Bangil, Pasuruan. Memiliki Motto "Berdamai Dengan Kecewa". Telah menyelesaikan Pendidikan S-2 Bimbingan dan Konseling di Universitas Negeri Malang.
 </template>
 
 <script>
